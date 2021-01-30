6f6e76e25b9d142071d06b97d91d728fddd122c8
snigdhaArya <76739977+snigdhaArya@users.noreply.github.com>
2021-01-30 18:48:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+const World=Matter.World;
+const Engine=Matter.Engine;
+const Bodies=Matter.Bodies;
+
+var engine,object,world,obectP,ball,ballP;
+
+function setup() {
+  createCanvas(400,400);
+  
+  engine=Engine.create();
+  world=engine.world;
+  obectP={isStatic:true}
+  ballP={restitution:1.0}
+  object = Bodies.rectangle(200,100,50,50,obectP);
+  
+   World.add(world,object);
+   console.log(object);
+ 
+   ball=Bodies.circle(100,100,30,ballP);
+   World.add(world,ball);
+   
+}
+
+function draw() {
+  background(0);  
+  Engine.update(engine);
+  rectMode(CENTER);
+  rect(200,200,50,50);
+  rect(object.position.x,object.position.y,100,100);
+  circle(ball.position.x,ball.position.y,30); 
+}
